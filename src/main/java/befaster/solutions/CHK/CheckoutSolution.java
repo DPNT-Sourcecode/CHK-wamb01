c680019d3387863847fdca9d9fa51ebaca208a35
sbx_user1051 <sbx_user1051@169.254.46.237>
2020-01-28 11:16:01 +0000

    Tue Jan 28 11:16:01 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,14 @@
 package befaster.solutions.CHK;
 
+import java.util.List;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+    	
+    	
         throw new SolutionNotImplementedException();
     }
 }
+
--- src/main/java/befaster/solutions/CHK/ItemPrice.java
+++ src/main/java/befaster/solutions/CHK/ItemPrice.java
@@ -1,5 +1,18 @@
 package befaster.solutions.CHK;
 
+import java.util.TreeMap;
+
 public class ItemPrice {
 
+	public static TreeMap<String, Integer> itemprice=new TreeMap<String, Integer>();
+	static {
+		
+		itemprice.put("A", 50);
+		itemprice.put("B", 30);
+		itemprice.put("C", 20);
+		itemprice.put("D", 15);
+
+
+	}
 }
+
