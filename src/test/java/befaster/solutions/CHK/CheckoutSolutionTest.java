1f31a326672a3c7851d0503597fc26372bc5d3d3
sbx_user1051 <sbx_user1051@169.254.46.237>
2020-01-28 11:13:01 +0000

    Tue Jan 28 11:13:01 UTC 2020

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -2,13 +2,20 @@ package befaster.solutions.CHK;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CheckoutSolutionTest {
 
+	private CheckoutSolution checkoutSolution;
+	@Before
+	public void setup() {
+		checkoutSolution=new CheckoutSolution();
+	}
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+		checkoutSolution.checkout("A");
 	}
 
 }
