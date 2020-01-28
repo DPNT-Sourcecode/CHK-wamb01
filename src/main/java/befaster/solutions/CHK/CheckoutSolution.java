86a957e340042007b01d2a479acc204225b6d7b2
sbx_user1051 <sbx_user1051@169.254.46.237>
2020-01-28 11:41:37 +0000

    Tue Jan 28 11:41:37 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,17 +7,28 @@ public class CheckoutSolution {
     	
     	 char[] items=skus.toCharArray();
     	 int totalprice=0;
-    	 int countA;
-    	 int countB;
+    	 int countA=0;
+    	 int countB=0;
     	 for(char c:items) {
     		 
     		 if(!ItemPrice.itemprice.containsKey(c))
     	    		return -1;
     		 else
+    			 {
+    			 if(c=='A') countA++;
+    			 if(c=='B') countB++;
     			 totalprice=totalprice+ItemPrice.itemprice.get(c);
+    			 }
+
+    	 }
+    	 if(countA==3) {
+    		 totalprice=totalprice-20;
+    	 }else if(countB==2){
+    		 totalprice=totalprice-15;
 
     	 }
     	 return totalprice;
     	 
     }
 }
+
