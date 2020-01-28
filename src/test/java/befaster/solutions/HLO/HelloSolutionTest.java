87c2568ce85d0ffdf0f5ac9abb8342d23d2d8ea5
sbx_user1051 <sbx_user1051@169.254.46.237>
2020-01-28 10:49:40 +0000

    Tue Jan 28 10:49:40 UTC 2020

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -3,6 +3,7 @@ package befaster.solutions.HLO;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class HelloSolutionTest {
@@ -13,10 +14,15 @@ public class HelloSolutionTest {
 		helloSolution=new HelloSolution();
 	}
 	@Test
+	@Ignore
 	public void test() {
 		assertEquals("Not Matched", "Hello, World!", helloSolution.hello("Praveen"));
 	}
+	public void test2() {
+		assertEquals("Not Matched", "Hello, Praveen!", helloSolution.hello("Praveen"));
+	}
 
 }
+
 
 
