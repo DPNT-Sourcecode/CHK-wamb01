6aad9a926bd4619e924c69d17f778a1359be48f5
sbx_user1051 <sbx_user1051@169.254.46.237>
2020-01-28 11:10:01 +0000

    Tue Jan 28 11:10:01 UTC 2020

---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -0,0 +1,14 @@
+package befaster.solutions.CHK;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CheckoutSolutionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
