0bf80e3cb715b68295848da8db09de6a808fd9e5
sbx_user1051 <sbx_user1051@169.254.46.237>
2020-01-28 10:40:00 +0000

    Tue Jan 28 10:40:00 UTC 2020

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,15 @@
+package befaster.solutions.HLO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloSolutionTest {
+
+	private HelloSolution helloSolution;
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
