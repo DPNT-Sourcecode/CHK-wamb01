497cd5c3a6ab402c9c6c489621087bd8827f0814
sbx_user1051 <sbx_user1051@169.254.123.141>
2020-01-31 23:31:31 +0000

    Fri Jan 31 23:31:31 UTC 2020

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -460,11 +460,14 @@ public void testSTXS() {
 	assertEquals(62,checkoutSolution.checkout("STXS").intValue());
 
 }
-/*
-{"method":"checkout","params":["SSSZ"],"id":"CHK_R5_142"}, expected: 65, got: 62
-- {"method":"checkout","params":["ZZZS"],"id":"CHK_R5_144"}, expected: 65, got: 62
-*/
+@Test
+public void testZZZS() {
+	assertEquals(65,checkoutSolution.checkout("ZZZS").intValue());
+
 }
+
+}
+
 
 
 
